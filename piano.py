16395ac0c9e5a43f40f82fde32d78a918803dbb5
cac824 <charlesac118@gmail.com>
2024-12-30 14:27:07 -0600

    Update piano.py
    added GUI

--- piano.py
+++ piano.py
@@ -2,15 +2,15 @@ import librosa
 import pyaudio
 import wave
 import sys
+import os
 import numpy as np
 import random
-
-total = 0
-count = 0
+import tkinter as tk
+from tkinter import ttk
 
 # list of notes
-piano_notes_list = ['C4','D4','E4','F4','G4','A4','B4','C4','C#4','D#4','F#4','G#4','A#4','Eb4']
-chords_dict = { # list of chords
+piano_notes_list = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C4', 'C#4', 'D#4', 'F#4', 'G#4', 'A#4', 'Eb4']
+chords_dict = {  # list of chords
     'Chord 1 (C E G)': ['C4', 'E4', 'G4'],
     'Chord 2 (D F# A)': ['D4', 'F#4', 'A4'],
     'Chord 3 (E G# B)': ['E4', 'G#4', 'B4'],
@@ -18,25 +18,25 @@ chords_dict = { # list of chords
     'Chord 5 (D F A)': ['D4', 'F4', 'A4'],
     'Chord 6 (E G B)': ['E4', 'G4', 'B4']
 }
+
 note_list = []
 unique_notes = []
 
-choice = input("Do you want to play one note or multiple notes O/M")
-if choice == 'O': # this is if you want to play one note
-    random_note = random.choice(piano_notes_list)
-    print("Play {}".format(random_note))
-elif choice == 'M': # checks if you want to play multiple notes
-    random_chord = random.choice(list(chords_dict.keys()))
-    random_chord_notes = chords_dict[random_chord]
-    print("Play {}".format(random_chord))
+# Global variables to store the generated note and chord
+generated_note = None
+generated_chord = None
 
-record = input("Enter R to record")
-if record == 'R':
+
+def record_audio():
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1 if sys.platform == 'darwin' else 2
     RATE = 44100
     RECORD_SECONDS = 5
+
+    # checks if the file exists then deletes it if it does
+    if os.path.exists('note_audio.wav'):
+        os.remove('note_audio.wav')
 
     with wave.open('note_audio.wav', 'wb') as wf:
         p = pyaudio.PyAudio()
@@ -46,47 +46,92 @@ if record == 'R':
 
         stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
 
-        print('Recording...')
         for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
             wf.writeframes(stream.read(CHUNK))
-        print('Done')
-else:
-    print("Wrong input try again")
-
-# ^^^^^ This deals with recording the audio
-
-y, sr = librosa.load('note_audio.wav') # loads audio file
-f0 = librosa.pyin(y, fmin=100, fmax=2093)
-freq = np.array(f0) # makes sure its a numpy array
-freq_no_nan = freq[~np.isnan(freq)] # get rids of all nan values
-
-# checks for any infinite values in the array and sets them to 0
-infinite_index = np.isinf(freq_no_nan)
-freq_no_nan[infinite_index] = 0
-# note detection
-valid_freqs = freq_no_nan[(freq_no_nan > 20) & (freq_no_nan < 4000)] # frequency range
-unique_freqs = np.unique(valid_freqs) # finds unique frequencies
-for i in unique_freqs: # adds the unique frequencies to the notes list
-    note_list.append(librosa.hz_to_note(i))
-
-note_list = [note for note in note_list if note in piano_notes_list] # removing any notes not in piano_notes_list w/ list comprehension
-for i in note_list: # removes any duplicates
-    if i not in unique_notes:
-        unique_notes.append(i)
 
 
-print(note_list)
-print(unique_notes)
-if choice == 'O':
-    print(random_note)
-    if unique_notes[0] == random_note:
-        print("Hooray you played the correct note!")
+def detect_notes():
+    global note_list
+    unique_notes.clear()
+    note_list.clear()
+
+    # loads audio file
+    y, sr = librosa.load('note_audio.wav')
+    f0 = librosa.pyin(y, fmin=100, fmax=2093)
+    freq = np.array(f0)  # makes sure it's a numpy array
+    freq_no_nan = freq[~np.isnan(freq)]  # get rid of all nan values
+
+    # check for any infinite values and set them to 0
+    infinite_index = np.isinf(freq_no_nan)
+    freq_no_nan[infinite_index] = 0
+
+    # note detection
+    valid_freqs = freq_no_nan[(freq_no_nan > 20) & (freq_no_nan < 4000)]  # frequency range
+    unique_freqs = np.unique(valid_freqs)  # find unique frequencies
+    for i in unique_freqs:  # add the unique frequencies to the notes list
+        note_list.append(librosa.hz_to_note(i))
+
+    note_list = [note for note in note_list if note in piano_notes_list]  # remove any notes not in piano_notes_list
+    for i in note_list:  # remove duplicates
+        if i not in unique_notes:
+            unique_notes.append(i)
+
+    return unique_notes
+
+
+def generate_note():
+    global generated_note  # Make sure to use the global variable
+    generated_note = random.choice(piano_notes_list)
+    note_label.config(text=generated_note)
+    return generated_note
+
+
+def generate_chord():
+    global generated_chord  # Make sure to use the global variable
+    random_chord = random.choice(list(chords_dict.keys()))
+    generated_chord = chords_dict[random_chord]
+    note_label.config(text=generated_chord)
+    return generated_chord
+
+
+def check_note():
+    detected_notes = detect_notes()
+
+    if generated_note is not None:
+        if detected_notes[0] == generated_note:
+            note_label.config(text="You played the correct note!")
+        else:
+            note_label.config(text="You played the wrong note")
     else:
-        print("You played the wrong note")
-elif choice == 'M':
-    print(random_chord)
-    if unique_notes == random_chord_notes:
-        print("Hooray you played the correct notes!")
-    else:
-        print("You played the wrong notes")
+        note_label.config(text="Please generate a note or chord first.")
 
+    if generated_chord is not None:
+        if detected_notes == generated_chord:
+            note_label.config(text="You played the correct chord!")
+        else:
+            note_label.config(text="You played the wrong chord!")
+
+
+window = tk.Tk()
+window.title("Piano")
+window.geometry("500x300")
+
+window_frame = tk.Frame(window, bg="white", width=250, height=250)
+window_frame.pack()
+
+note_label = tk.Label(window_frame, bg="white", text="Notes will appear here", font=("Arial", "15"))
+note_label.pack(pady=50, padx=50)
+
+onenote_button = ttk.Button(window, text="Generate one note", width=20, command=generate_note)
+onenote_button.pack()
+
+multiplenote_button = ttk.Button(window, text="Generate a chord", width=20, command=generate_chord)
+multiplenote_button.pack()
+
+checknote_button = ttk.Button(window, text="Check Notes", width=20, command=check_note)
+checknote_button.pack()
+
+record_button = ttk.Button(window, text="Record", command=record_audio, width=20)
+record_button.pack()
+
+window.mainloop()
